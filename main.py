5fcc2aee5b8e668acbcb28c36ba8b79b964a865e
Vitório Aflalo <vitorioaflalo86@live.com>
2023-01-20 21:51:07 -0300

    Criado função de exibição de mensagem

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def gether_data():
 
     return n1, n2
 
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} somados dão {n1+n2}')
+    
+    return None
+
 def main():
     n1, n2 = gether_data()
 
