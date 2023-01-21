3611833c42b286aeb3eddbf28ff896ad0cce14d8
Vitório Aflalo <vitorioaflalo86@live.com>
2023-01-20 21:22:19 -0300

    Adicionada função de coleta de dados

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def gether_data():
+    n1 = int(input('Primeiro valor: '))
+    n2 = n1 = int(input('Segundo valor: '))
+
+    return n1, n2
+
 def main():
-    print(2*2)
+    n1, n2 = gether_data()
+
+    print(n1*n2)
+
     return None
 
 if __name__ == "__main__":
